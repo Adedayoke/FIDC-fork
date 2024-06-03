f3270802c4e05f0476bc5b148768debb1f9f2a1e
Adedayoke <adedayoke2006@gmail.com>
2024-06-03 17:52:18 +0100

    css update

--- flask/static/script.js
+++ flask/static/script.js
@@ -1,41 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
     const dashboard = document.querySelector('.dashboard')
-    const profileBtn = dashboard.querySelector('.profile');
-    const profileSection = dashboard.querySelector('.profileSection');
-    const updateBtn = dashboard.querySelector('.update');
-    const updateSection = dashboard.querySelector('.updateSection');
-    const tHistoryBtn = dashboard.querySelector('.tHistory');
-    const thistorySection = dashboard.querySelector('.tHistorySection');
+    const profileBtn = document.querySelector('.profile');
+    const profileSection = document.querySelector('.profileSection');
+    const updateBtn = document.querySelector('.update');
+    const updateSection = document.querySelector('.updateSection');
+    const tHistoryBtn = document.querySelector('.tHistory');
+    const thistorySection = document.querySelector('.tHistorySection');
     const alert = document.querySelectorAll('.alert');
     const ham = document.querySelector('.ham');
     const sidebar = document.querySelector('.sidebar');
     
-    profileBtn.addEventListener('click', function() {
-      profileBtn.classList.add("activedash")
-      updateBtn.classList.remove("activedash")
-      tHistoryBtn.classList.remove("activedash")
-      profileSection.classList.remove("sectNone")
-      updateSection.classList.add("sectNone")
-      thistorySection.classList.add("sectNone")
-    });
+    if (profileBtn){
+      profileBtn.addEventListener('click', function() {
+        profileBtn.classList.add("activedash")
+        updateBtn.classList.remove("activedash")
+        tHistoryBtn.classList.remove("activedash")
+        profileSection.classList.remove("sectNone")
+        updateSection.classList.add("sectNone")
+        thistorySection.classList.add("sectNone")
+      });
+    }
   
-    updateBtn.addEventListener('click', function() {
-      updateBtn.classList.add("activedash")
-      profileBtn.classList.remove("activedash")
-      tHistoryBtn.classList.remove("activedash")
-      updateSection.classList.remove("sectNone")
-      profileSection.classList.add("sectNone")
-      thistorySection.classList.add("sectNone")
-    });
+    if(updateBtn){
+      updateBtn.addEventListener('click', function() {
+        updateBtn.classList.add("activedash")
+        profileBtn.classList.remove("activedash")
+        tHistoryBtn.classList.remove("activedash")
+        updateSection.classList.remove("sectNone")
+        profileSection.classList.add("sectNone")
+        thistorySection.classList.add("sectNone")
+      });
+    }
   
-    tHistoryBtn.addEventListener('click', function() {
-      tHistoryBtn.classList.add("activedash")
-      profileBtn.classList.remove("activedash")
-      updateBtn.classList.remove("activedash")
-      updateSection.classList.add("sectNone")
-      profileSection.classList.add("sectNone")
-      thistorySection.classList.remove("sectNone")
-    }); 
+    if (tHistoryBtn){
+      tHistoryBtn.addEventListener('click', function() {
+        tHistoryBtn.classList.add("activedash")
+        profileBtn.classList.remove("activedash")
+        updateBtn.classList.remove("activedash")
+        updateSection.classList.add("sectNone")
+        profileSection.classList.add("sectNone")
+        thistorySection.classList.remove("sectNone")
+      }); 
+    }
     alert.forEach((alt)=>{
       setTimeout(()=>{
         alt.classList.add("altNone")
